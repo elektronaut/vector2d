74a4748c1d0ef77b4e2ff5d61f3f10cd017e608b
Inge Jørgensen <inge@manualdesign.no>
2014-06-20 03:23:15 +0200

    Added #contain method

--- lib/vector2d/fitting.rb
+++ lib/vector2d/fitting.rb
@@ -2,7 +2,20 @@
 
 class Vector2d
   module Fitting
-    # Returns a new vector fitting inside another vector, retaining the aspect ratio.
+
+    # Scales down the given vector unless it fits inside.
+    #
+    #   vector = Vector2d(20, 20)
+    #   vector.contain(Vector2d(10, 10)) # => Vector2d(10,10)
+    #   vector.contain(Vector2d(40, 20)) # => Vector2d(20,10)
+    #   vector.contain(Vector2d(20, 40)) # => Vector2d(10,20)
+    #
+    def contain(other)
+      v, _ = coerce(other)
+      (v.x > x || v.y > y) ? other.fit(self) : other
+    end
+
+    # Scales the vector to fit inside another vector, retaining the aspect ratio.
     #
     #   vector = Vector2d(20, 10)
     #   vector.fit(Vector2d(10, 10)) # => Vector2d(10,5)
--- spec/lib/vector2d/fitting_spec.rb
+++ spec/lib/vector2d/fitting_spec.rb
@@ -5,6 +5,34 @@ require 'spec_helper'
 describe Vector2d::Fitting do
   let(:original) { Vector2d.new(300, 300) }
 
+  describe "#contain" do
+    subject(:vector) { original.contain(comp) }
+
+    context "when vector is smaller" do
+      let(:comp) { Vector2d.new(150, 100) }
+      it "should not change it" do
+        expect(vector.x).to eq(150)
+        expect(vector.y).to eq(100)
+      end
+    end
+
+    context "when vector is wider" do
+      let(:comp) { Vector2d.new(400, 300) }
+      it "should scale it down" do
+        expect(vector.x).to eq(300)
+        expect(vector.y).to eq(225)
+      end
+    end
+
+    context "when vector is higher" do
+      let(:comp) { Vector2d.new(300, 400) }
+      it "should scale it down" do
+        expect(vector.x).to eq(225)
+        expect(vector.y).to eq(300)
+      end
+    end
+  end
+
   describe "#fit" do
     subject(:vector) { original.fit(comp) }
 
@@ -25,7 +53,7 @@ describe Vector2d::Fitting do
     end
   end
 
-  describe "#contrain_one" do
+  describe "#fit_either" do
     let(:original) { Vector2d.new(300, 300) }
     subject(:vector) { original.fit_either(comp) }
 
